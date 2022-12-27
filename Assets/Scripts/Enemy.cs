867610f0a60d9f21f00aaa4f5becbeeab752066a
MelDxKviel <mega1hack52@gmail.com>
2022-12-28 03:00:45 +0700

    Remove unused assets, rework scripts

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,32 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public float hp;
+    public Gradient gradient;
+    
+    private float maxHp;
+    
+    private void Update()
+    {
+        
+    }
 
     public void TakeDamage(float damage)
     {
         hp -= damage;
-        GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.white, hp / 100);
+        GetComponent<Renderer>().material.color = gradient.Evaluate(Mathf.InverseLerp(0, maxHp, hp));
         if (hp <= 0)
         {
             Destroy(gameObject);
             FindObjectOfType<Score>().IncreaseScore(1);
         }
+    }
+
+    private void Start()
+    {
+        maxHp = hp;
     }
 
     private void OnTriggerEnter2D(Collider2D col)
